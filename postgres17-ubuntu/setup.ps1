1a6c3097ac87a50d087d078b8a37963df7dadbe8
Özkan Pakdil <ozkan.pakdil@red-gate.com>
2024-11-01 13:13:41 +0000

    commands moved to podman

--- postgres17-ubuntu/setup.ps1
+++ postgres17-ubuntu/setup.ps1
@@ -1,4 +1,6 @@
-docker build --no-cache  -t postgresubuntu17 .
-docker run -dit --name postgresubuntu17 -p 5432:5432 -p 22:22 postgresubuntu17
-docker cp postgresubuntu17:/root/.ssh/id_rsa ./root.key
+podman build --no-cache  -t postgresubuntu17 .
+podman run -dit --name postgresubuntu17 -p 5432:5432 -p 22:22 postgresubuntu17
+podman cp postgresubuntu17:/root/.ssh/id_rsa ./root.key
 ssh -i root.key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@localhost
+# in case of ssh issues run command below
+# podman exec -it postgresubuntu17 bash
