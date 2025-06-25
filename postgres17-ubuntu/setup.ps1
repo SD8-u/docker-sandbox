4cc726281ef4606c7a52091861980464f5611cad
Özkan Pakdil <ozkan.pakdil@red-gate.com>
2025-06-25 14:53:33 +0100

    dosunix and small improvement on host internet connection

--- postgres17-ubuntu/setup.ps1
+++ postgres17-ubuntu/setup.ps1
@@ -1,4 +1,4 @@
-podman build --no-cache  -t postgresubuntu17 .
+podman build --no-cache --network=host -t postgresubuntu17 .
 podman run -dit --name postgresubuntu17 --cap-add SYS_CHROOT --cap-add AUDIT_WRITE --cap-add CAP_NET_RAW -p 5432:5432 -p 22:22 postgresubuntu17
 podman cp postgresubuntu17:/root/.ssh/id_rsa ./root.key
 ssh -i root.key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@localhost
